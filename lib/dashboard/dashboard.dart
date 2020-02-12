2e5b3c9cad568f520bea5535a03efa61162ba3fb
Maximilian Rauh <inf2802@hs-worms.de>
2020-02-12 15:15:32 +0100

    added dashboard folder and ini data

---
+++ lib/dashboard/dashboard.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  DashboardState createState() => DashboardState();
+}
+
+class DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Modul hinzufügen',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
