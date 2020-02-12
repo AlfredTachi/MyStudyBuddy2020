d8095088b3b49b9bcbf79a6f39963e64338b790b
Maximilian Rauh <inf2802@hs-worms.de>
2020-02-12 16:54:00 +0100

    first working dashboard sample

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -6,17 +7,60 @@ class Dashboard extends StatefulWidget {
 }
 
 class DashboardState extends State<Dashboard> {
+  Map<String, double> dataMap = new Map();
+
+  @override
+  void initState() {
+    dataMap.putIfAbsent("Flutter", () => 5);
+    dataMap.putIfAbsent("React", () => 3);
+    dataMap.putIfAbsent("Xamarin", () => 2);
+    dataMap.putIfAbsent("Ionic", () => 2);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dashboard"),
+        title: Text("MyStudyBuddy"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: Column(
+        children: <Widget>[
+          Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    width: 200,
+                    padding: EdgeInsets.all(15),
+                    child: PieChart(
+                      dataMap: dataMap,
+                      showLegends: false,
+                      chartType: ChartType.ring,
+                      showChartValues: false,
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[Icon(Icons.headset), Text("0")],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.headset),
+                          Text("Noch keinen")
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[Icon(Icons.person), Text("inf2802")],
+                      )
+                    ],
+                  )
+                ],
+              )),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
