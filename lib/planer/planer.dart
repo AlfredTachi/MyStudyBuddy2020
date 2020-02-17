28a6797de8291b4d0f5e9b764ddb134fa4f67689
Felix Roskos <inf2730@hs-worms.de>
2020-02-17 16:18:59 +0100

    planerbasic

---
+++ lib/planer/planer.dart
@@ -0,0 +1,63 @@
+import 'package:MyStudyBuddy2/drawer/drawer.dart';
+import 'package:flutter/material.dart';
+
+class Planer extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+
+    return _PlanerState();
+  }
+}
+
+class _PlanerState extends State<Planer>{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(title:Text("Terminplaner")),
+      drawer: OwnDrawer(),
+      body:makeBody
+
+    );
+  }
+}
+final makeBody = Container(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard;
+        },
+      ),
+    );
+    final makeCard = Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        child: makeListTile,
+      ),
+    );
+
+    final makeListTile = ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),
+          child: Icon(Icons.autorenew, color: Colors.white),
+        ),
+        title: Text(
+          "Introduction to Driving",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Row(
+          children: <Widget>[
+            Icon(Icons.linear_scale, color: Colors.yellowAccent),
+            Text(" Intermediate", style: TextStyle(color: Colors.white))
+          ],
+        ),
+        trailing:
+            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
