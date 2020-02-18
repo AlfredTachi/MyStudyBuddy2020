e60bc810230c4f0c96be7049a1abbbf3f510a893
Felix Roskos <inf2730@hs-worms.de>
2020-02-18 10:43:51 +0100

    planer added

--- lib/planer/planer.dart
+++ lib/planer/planer.dart
@@ -1,4 +1,6 @@
 import 'package:MyStudyBuddy2/drawer/drawer.dart';
+import 'package:MyStudyBuddy2/planer/planer_list.dart';
+import 'package:MyStudyBuddy2/planer/upcoming_events.dart';
 import 'package:flutter/material.dart';
 
 class Planer extends StatefulWidget{
@@ -39,37 +41,39 @@ TabController _tabController;
       body:TabBarView(
         controller: _tabController,
         children: <Widget>[
-          makeBody,
-          Text('whatever')
+          PlanerList(),
+          EventList()
         ],
       )
 
     );
-  }
-}
-final makeBody = Container(
-      child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return makeCard;
-        },
-      ),
-    );
-    final makeCard = Card(
-      elevation: 8.0,
-      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-        child: makeListTile,
-      ),
-    );
+  }}
 
-    final makeListTile = ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        leading: Container(
-          child:Text('test')
-        ),
-        trailing: Icon(Icons.favorite),
-        );
+// }
+// final makeBody = Container(
+//       child: ListView.builder(
+//         scrollDirection: Axis.vertical,
+//         shrinkWrap: true,
+//         itemCount: 10,
+//         itemBuilder: (BuildContext context, int index) {
+//           return makeCard;
+//         },
+//       ),
+//     );
+//     final makeCard = Card(
+//       elevation: 8.0,
+//       margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+//       child: Container(
+//         decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+//         child: makeListTile,
+//       ),
+//     );
+
+    // final makeListTile = ListTile(
+
+    //     contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+    //     leading: Container(
+    //       child:Text('test')
+    //     ),
+    //     trailing: Icon(Icons.favorite),
+    //     );
