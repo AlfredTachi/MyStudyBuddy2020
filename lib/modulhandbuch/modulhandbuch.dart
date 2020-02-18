de19b9fc1e69fbae1f0e6b725327824142efef86
Maximilian Rauh <inf2802@hs-worms.de>
2020-02-18 16:18:27 +0100

    Modulehandbook added FutureBuilder

--- lib/modulhandbuch/modulhandbuch.dart
+++ lib/modulhandbuch/modulhandbuch.dart
@@ -5,51 +5,56 @@ import 'package:MyStudyBuddy2/drawer/drawer.dart';
 
 class ModuleHandbooks extends StatefulWidget {
   @override
-   State<StatefulWidget> createState() => ModuleHandbooksState();
+  State<StatefulWidget> createState() => ModuleHandbooksState();
 }
 
 class ModuleHandbooksState extends State<ModuleHandbooks> {
-  PDFDocument document, documentAsset;
-  bool isLoaded = false;
+  PDFDocument document;
+  bool isLoading = true;
 
-  _loadPdf() async {
-    document =await PDFDocument.fromURL("https://hswocloud.hs-worms.de/hswocloud/index.php/s/9j6tyjaazmFKnNC/download?path=%2F&files=AnInf_Modulhandbuch_2019w.pdf");
-    setState(() { 
-      isLoaded = true;
-    });
+  Future<void> _loadPdf() async {
+    document = await PDFDocument.fromURL(
+        "https://hswocloud.hs-worms.de/hswocloud/index.php/s/9j6tyjaazmFKnNC/download?path=%2F&files=AnInf_Modulhandbuch_2019w.pdf");
+    return document;
   }
 
   @override
   void initState() {
-    super.initState();    
-    _loadPdf();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("MyStudyBuddy"),
-      ),
-      drawer: OwnDrawer(),
-      body: Center(
+        appBar: AppBar(
+          title: Text("MyStudyBuddy"),
+        ),
+        drawer: OwnDrawer(),
+        body: Center(
           child: Column(
-            children: <Widget>[              
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.85,
-                  child: PDFViewer(
-                    document: document,
-                  ),
-                )              
-             
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height * 0.85,
+                child: FutureBuilder(
+                  future: _loadPdf(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return PDFViewer(
+                        document: document,
+                      );
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+              )
             ],
-          ),)
-    );
+          ),
+        ));
   }
 }
 
 class DocumentControl extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Text("Yihu");
