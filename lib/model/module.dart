2e88768866ad70a9188948e4321866d70900c621
Maximilian Rauh <rauh.maximilian@gmail.com>
2020-02-24 15:54:23 +0100

    added border shape

--- lib/model/module.dart
+++ lib/model/module.dart
@@ -13,26 +13,23 @@ class Module {
   );
 
   Widget module() {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
+    return Container(
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(15.0),
         ),
-        child: RaisedButton(
-          color: Color(0xFF013D62),
-          onPressed: () {
-            if (_isSelected) {
-              ///Open Alert Dialog
-            } else {
-              ModuleController().addModule(this);
-              _isSelected = true;
-            }
-          },
-          child: Center(
-              child: Text(title,
-                  style: TextStyle(fontSize: 20, color: Colors.white))),
-        ),
+        color: Color(0xFF013D62),
+        onPressed: () {
+          if (_isSelected) {
+            ///Open Alert Dialog
+          } else {
+            ModuleController().addModule(this);
+            _isSelected = true;
+          }
+        },
+        child: Center(
+            child: Text(title,
+                style: TextStyle(fontSize: 20, color: Colors.white))),
       ),
     );
   }
