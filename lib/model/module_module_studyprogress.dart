345c202ee31d7029098cacdd293af1408ddc3123
mscherzberg <ar-michael.scherzberg@rakuten.com>
2020-02-26 09:53:36 +0100

    fixed naming errors

---
+++ lib/model/module_module_studyprogress.dart
@@ -0,0 +1,41 @@
+import 'package:MyStudyBuddy2/singleton/module_controller.dart';
+import 'package:flutter/material.dart';
+
+class Module {
+  int id;
+  String title;
+  bool _isSelected = false;
+
+  Module(
+    this.id,
+    this.title,
+    // Function myModules
+  );
+
+  Widget module() {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        height: 80,
+        width: 80,
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    child: Center(
+                      child: Text(title,
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                    ),
+                  ),
+                ),
+              ]),
+            ],
+          ),
+        )));
+  }
+}
