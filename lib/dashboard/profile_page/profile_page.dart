c7426e459f58433c57b58b4efbf3a94fe418b7fd
Maximilian Rauh <inf2802@hs-worms.de>
2020-03-04 13:45:02 +0100

    fixed overflow

--- lib/dashboard/profile_page/profile_page.dart
+++ lib/dashboard/profile_page/profile_page.dart
@@ -70,122 +70,127 @@ class ProfilePageState extends State<ProfilePage> {
                       flex: 3,
                       child: Padding(
                         padding: const EdgeInsets.only(top: 0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 20, right: 20, bottom: 5),
-                              child: TextField(
-                                onChanged: (_val) {
-                                  ProfileController().saveData();
-                                },
-                                controller: ProfileController()
-                                    .getInfNumberController(),
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                                decoration: InputDecoration(
-                                    fillColor: Colors.white.withAlpha(40),
-                                    filled: true,
-                                    hintStyle: TextStyle(color: Colors.black45),
-                                    hintText: "Inf Nummer",
-                                    enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 1),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 2),
-                                    ),
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 1),
-                                    )),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 20, right: 20, bottom: 5),
-                              child: TextField(
-                                onChanged: (_val) {
-                                  ProfileController().saveData();
-                                },
-                                maxLength: 6,
-                                keyboardType: TextInputType.number,
-                                controller:
-                                    ProfileController().getMatrikelController(),
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                                decoration: InputDecoration(
-                                    counterText: "",
-                                    fillColor: Colors.white.withAlpha(40),
-                                    filled: true,
-                                    hintStyle: TextStyle(color: Colors.black45),
-                                    hintText: "Matrikel Nummer",
-                                    enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 1),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 2),
-                                    ),
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 1),
-                                    )),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 20, right: 20, bottom: 5),
-                              child: TextField(
-                                maxLines: null,
-                                onTap: () {
-                                  Navigator.of(context)
-                                      .pushNamed("/qspinfo")
-                                      .whenComplete(() {
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 20, right: 20, bottom: 5, top: 50),
+                                child: TextField(
+                                  onChanged: (_val) {
                                     ProfileController().saveData();
-                                  });
-                                },
-                                readOnly: true,
-                                keyboardType: TextInputType.number,
-                                controller:
-                                    ProfileController().getQSPController(),
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                                decoration: InputDecoration(
-                                    fillColor: Colors.white.withAlpha(40),
-                                    filled: true,
-                                    hintStyle: TextStyle(
-                                        color: Colors.black45, fontSize: 16),
-                                    hintText:
-                                        "Klick mich um ein QSP zu wählen!",
-                                    enabledBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 1),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 2),
-                                    ),
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                      borderSide: BorderSide(
-                                          color: Colors.white, width: 1),
-                                    )),
+                                  },
+                                  controller: ProfileController()
+                                      .getInfNumberController(),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                  decoration: InputDecoration(
+                                      fillColor: Colors.white.withAlpha(40),
+                                      filled: true,
+                                      hintStyle:
+                                          TextStyle(color: Colors.black45),
+                                      hintText: "Inf Nummer",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 1),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 2),
+                                      ),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 1),
+                                      )),
+                                ),
                               ),
-                            ),
-                          ],
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 20, right: 20, bottom: 5),
+                                child: TextField(
+                                  onChanged: (_val) {
+                                    ProfileController().saveData();
+                                  },
+                                  maxLength: 6,
+                                  keyboardType: TextInputType.number,
+                                  controller: ProfileController()
+                                      .getMatrikelController(),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                  decoration: InputDecoration(
+                                      counterText: "",
+                                      fillColor: Colors.white.withAlpha(40),
+                                      filled: true,
+                                      hintStyle:
+                                          TextStyle(color: Colors.black45),
+                                      hintText: "Matrikel Nummer",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 1),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 2),
+                                      ),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 1),
+                                      )),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 20, right: 20, bottom: 5),
+                                child: TextField(
+                                  maxLines: null,
+                                  onTap: () {
+                                    Navigator.of(context)
+                                        .pushNamed("/qspinfo")
+                                        .whenComplete(() {
+                                      ProfileController().saveData();
+                                    });
+                                  },
+                                  readOnly: true,
+                                  keyboardType: TextInputType.number,
+                                  controller:
+                                      ProfileController().getQSPController(),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                  decoration: InputDecoration(
+                                      fillColor: Colors.white.withAlpha(40),
+                                      filled: true,
+                                      hintStyle: TextStyle(
+                                          color: Colors.black45, fontSize: 16),
+                                      hintText:
+                                          "Klick mich um ein QSP zu wählen!",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 1),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 2),
+                                      ),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: BorderSide(
+                                            color: Colors.white, width: 1),
+                                      )),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
