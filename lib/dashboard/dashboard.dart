0d31b1157ba0215311f0a93fd2c93865042d00fd
Maximilian Rauh <inf2802@hs-worms.de>
2020-03-04 14:59:40 +0100

    changed edgeinset

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -2,9 +2,7 @@ import 'package:MyStudyBuddy2/local_database/local_database.dart';
 import 'package:MyStudyBuddy2/singleton/profile_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
-
 import '../local_database/local_database.dart';
-import '../singleton/module_controller.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -16,7 +14,7 @@ class DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     double spacing = MediaQuery.of(context).size.width / 1.8;
     return SafeArea(
-          child: Scaffold(
+      child: Scaffold(
         floatingActionButton: FloatingActionButton(
           backgroundColor: Color(0xFF013D62),
           onPressed: () {
@@ -27,81 +25,79 @@ class DashboardState extends State<Dashboard> {
           child: Icon(Icons.add),
         ),
         body: Column(
-            children: <Widget>[
-              Align(
-                  alignment: Alignment.topRight,
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.person,
-                      color: Color(0xCC013D62),
+          children: <Widget>[
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.person,
+                    color: Color(0xCC013D62),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed("/profilePage");
+                  },
+                )),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SizedBox(
+                  height: spacing,
+                  width: spacing,
+                  child: LiquidCircularProgressIndicator(
+                    value: ProfileController().getEarnedCP() /
+                        ProfileController().getMaxCP(),
+                    valueColor: AlwaysStoppedAnimation(
+                      Color(0xAA013D62),
                     ),
-                    onPressed: () {
-                      Navigator.of(context).pushNamed("/profilePage");
-                    },
-                  )),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: SizedBox(
-                    height: spacing,
-                    width: spacing,
-                    child: LiquidCircularProgressIndicator(
-                      value: ProfileController().getEarnedCP() /
-                          ProfileController().getMaxCP(), // Defaults to 0.5.
-                      valueColor: AlwaysStoppedAnimation(
-                        Color(0xAA013D62),
-                      ), // Defaults to the current Theme's accentColor.
-                      backgroundColor: Colors
-                          .white, // Defaults to the current Theme's backgroundColor.
-                      borderColor: Color(0xCC013D62),
-                      borderWidth: 5.0,
-                      direction: Axis
-                          .vertical, // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
-                      center: Text(
-                        ProfileController().getProgressText(),
-                        style: TextStyle(
-                            color: Colors.blueGrey[900],
-                            fontWeight: FontWeight.bold),
-                      ),
+                    backgroundColor: Colors.white,
+                    borderColor: Color(0xCC013D62),
+                    borderWidth: 5.0,
+                    direction: Axis.vertical,
+                    center: Text(
+                      ProfileController().getProgressText(),
+                      style: TextStyle(
+                          color: Colors.blueGrey[900],
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
               ),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.vertical(top: Radius.circular(30)),
-                      gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                          Colors.orange,
-                          Colors.deepOrange,
-                        ],
-                      )),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Padding(
-                                padding:
-                                    EdgeInsets.only(left: 15, top: 5, bottom: 15),
-                                child: Text(
-                                  "Meine Module",
-                                  style: TextStyle(
-                                      fontSize: 25, color: Colors.black54),
-                                ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(30)),
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Colors.orange,
+                        Colors.deepOrange,
+                      ],
+                    )),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  EdgeInsets.only(left: 15, top: 5, bottom: 15),
+                              child: Text(
+                                "Meine Module",
+                                style: TextStyle(
+                                    fontSize: 25, color: Colors.black54),
                               ),
-                            ],
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(left: 5, right: 5),
-                            alignment: Alignment.topLeft,
-                            child: Center(
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 5, right: 5),
+                          alignment: Alignment.topLeft,
+                          child: Center(
 /*                                   child: FutureBuilder(
                                       future: getFutureData(),
                                       builder: (BuildContext context,
@@ -116,30 +112,30 @@ class DashboardState extends State<Dashboard> {
                                         //   );
                                         // } else { 
                                         return */
-                              child: Center(
-                                child: Align(
-                                  heightFactor: 5,
-                                  child: FittedBox(
-                                    child: Text(
-                                      "Du hast zurzeit keine Module geplant!",
-                                      style: TextStyle(color: Colors.black45),
-                                    ),
+                            child: Center(
+                              child: Align(
+                                heightFactor: 5,
+                                child: FittedBox(
+                                  child: Text(
+                                    "Du hast zurzeit keine Module geplant!",
+                                    style: TextStyle(color: Colors.black45),
                                   ),
                                 ),
-                                //);
-                                // }
-                                // },
                               ),
+                              //);
+                              // }
+                              // },
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
     );
   }
