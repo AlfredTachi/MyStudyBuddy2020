bdf79abd80076642453531ef037fd79ec44fce1d
Felix Roskos <inf2730@hs-worms.de>
2020-03-06 10:50:15 +0100

    add grade box is sized

--- lib/exam_results/add_grade.dart
+++ lib/exam_results/add_grade.dart
@@ -2,6 +2,7 @@ import 'package:MyStudyBuddy2/model/module.dart';
 import 'package:MyStudyBuddy2/singleton/module_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:path/path.dart';
 
 Future<Widget> addGrade(Module module) async {
   TextEditingController _gradeCtrl = TextEditingController();
@@ -13,35 +14,40 @@ Future<Widget> addGrade(Module module) async {
   return Get.dialog(
     AlertDialog(
         title: const Text('Note eintragen!'),
-        content: Column(
-          children: [
-            TextField(
-              onChanged: (_val) {
-                grade = _val;
-              },
-              controller: _gradeCtrl,
-              decoration: InputDecoration(counterText: ""),
-              maxLength: 3,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
+        content: FractionallySizedBox(
+          heightFactor: 0.55,
+          child: Container(
+            child: Column(
+              children: [
+                TextField(
+                  onChanged: (_val) {
+                    grade = _val;
+                  },
+                  controller: _gradeCtrl,
+                  decoration: InputDecoration(counterText: ""),
+                  maxLength: 3,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+                OutlineButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: Text("Zurück")),
+                OutlineButton(
+                    onPressed: () {
+                      if (double.parse(grade) > 4.0) {
+                        Get.snackbar(
+                            "Fehler", "Korrekte Note (bis 4.0) eintragen.");
+                        disposeData();
+                      } else
+                        update(double.parse(grade), module);
+                      disposeData();
+                      Get.back();
+                    },
+                    child: Text("Speichern"))
+              ],
             ),
-            OutlineButton(
-                onPressed: () {
-                  Get.back();
-                },
-                child: Text("Zurück")),
-            OutlineButton(
-                onPressed: () {
-                  if (double.parse(grade) > 4.0) {
-                    Get.snackbar(
-                        "Fehler", "Korrekte Note (bis 4.0) eintragen.");
-                    disposeData();
-                  } else
-                    update(double.parse(grade), module);
-                  disposeData();
-                  Get.back();
-                },
-                child: Text("Speichern"))
-          ],
+          ),
         )),
   );
 }
