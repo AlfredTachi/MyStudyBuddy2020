a55e2d0aea69d41aed7a2ffc805a5ea6d2610ea2
Julia Henrich <inf2671@hs-worms.de>
2020-03-11 15:14:46 +0100

    new questions, changed design result page, changed result logic

--- lib/study_quiz/resultpage.dart
+++ lib/study_quiz/resultpage.dart
@@ -3,13 +3,14 @@ import 'package:MyStudyBuddy2/study_quiz/home.dart';
 
 class Resultpage extends StatefulWidget {
   int marks;
-  Resultpage({Key key , @required this.marks}) : super(key : key);
+  var mydatatemp;
+  Resultpage({Key key, @required this.marks, @required this.mydatatemp})
+      : super(key: key);
   @override
-  _ResultpageState createState() => _ResultpageState(marks);
+  _ResultpageState createState() => _ResultpageState(marks, mydatatemp);
 }
 
 class _ResultpageState extends State<Resultpage> {
-
   List<String> images = [
     "assets/images/success.png",
     "assets/images/good.png",
@@ -20,44 +21,68 @@ class _ResultpageState extends State<Resultpage> {
   String image;
 
   @override
-  void initState(){
-    if(marks < 3){
+  void initState() {
+    int totalquestions = mydatatemp.length;
+    int medium = (totalquestions * 0.7).toInt();
+    int bad = (totalquestions * 0.4).toInt();
+
+    print(medium);
+    print(bad);
+    if (marks <= bad) {
       image = images[2];
-      message = "Versuchs nochmal.\n" + "Dein Score $marks";
-    }else if(marks < 6){
+      message = "Du hast $marks/$totalquestions Punkten erreicht\n" +
+          "Versuchs nochmal.";
+    } else if (marks <= medium) {
       image = images[1];
-      message = "Das geht noch besser...\n" + "Dein Score $marks";
-    }else{
+      message = "Du hast $marks/$totalquestions Punkten erreicht\n" +
+          "Das geht noch besser...";
+    } else {
       image = images[0];
-      message = "Sehr gut :)\n" + "Dein Score $marks";
+      message =
+          "Du hast $marks/$totalquestions Punkten erreicht\n" + "Sehr gut :)";
     }
     super.initState();
   }
 
   int marks;
-  _ResultpageState(this.marks);
+  var mydatatemp;
+  _ResultpageState(this.marks, this.mydatatemp);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Ergebnis",
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 8,
-            child: Material(
-              elevation: 10.0,
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Material(
-                      child: Container(
-                        width: 300.0,
-                        height: 300.0,
-                        child: ClipRect(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: OutlineButton(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 3, bottom: 3),
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 36,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                  )),
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => Homepage(),
+                    ));
+                  }),
+            ),
+            Expanded(
+              child: Material(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Material(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height * 0.7,
                           child: Image(
                             image: AssetImage(
                               image,
@@ -65,56 +90,27 @@ class _ResultpageState extends State<Resultpage> {
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 5.0,
-                        horizontal: 15.0,
-                      ),
-                      child: Center(
-                      child: Text(
-                        message,
-                        style: TextStyle(
-                          fontSize: 18.0,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    )
-                    ),
-                  ],
+                      Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 5.0,
+                            horizontal: 15.0,
+                          ),
+                          child: Center(
+                            child: Text(
+                              message,
+                              style: TextStyle(
+                                fontSize: 22.0,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          )),
+                    ],
+                  ),
                 ),
               ),
-            ),            
-          ),
-          Expanded(
-            flex: 4,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                OutlineButton(
-                  onPressed: (){
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => Homepage(),
-                    ));
-                  },
-                  
-                  child: Text(
-                    "Zurück zum Start",
-                    style: TextStyle(
-                      fontSize: 18.0,
-                    ),
-                  ),
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 25.0,
-                  ),
-                  borderSide: BorderSide(width: 3.0, color: Colors.orange),
-                  splashColor: Colors.orangeAccent,
-                )
-              ],
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
