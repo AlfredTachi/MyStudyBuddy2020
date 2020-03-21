75ed9162bea840c1a72edbc6240f9aa69a0f7b22
Jonas Abrahams <jonasabrahams@icloud.com>
2020-03-21 10:44:22 +0100

    refactor grade list

--- lib/grades_prognosis/grades_list.dart
+++ lib/grades_prognosis/grades_list.dart
@@ -16,79 +16,6 @@ class GradesList extends StatefulWidget {
 class GradesListState extends State<GradesList> {
   @override
   Widget build(BuildContext context) {
-    List<Widget> getColumnChildren() {
-      List<Widget> _list = List<Widget>();
-      if (!Platform.isIOS) {
-        _list.add(
-          Container(
-            color: Colors.orange,
-            child: Row(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: OutlineButton(
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 3, bottom: 3),
-                        child: Icon(
-                          Icons.arrow_back,
-                          size: 36,
-                        ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(15),
-                        bottomRight: Radius.circular(15),
-                      )),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      }),
-                ),
-                Align(
-                    alignment: Alignment.topCenter,
-                    child: Padding(
-                        padding:
-                            const EdgeInsets.only(left: 20, top: 3, bottom: 3),
-                        child: Text(
-                          "Notenliste",
-                          style: TextStyle(fontSize: 25),
-                        ))),
-              ],
-            ),
-          ),
-        );
-      }
-      if (Platform.isIOS) {
-        _list.add(Container(
-          color: Styles.scaffoldBackground,
-          child: CustomScrollView(
-            slivers: <Widget>[
-              SliverSafeArea(
-                top: true,
-                sliver: SliverList(
-                  delegate: SliverChildListDelegate(
-                    <Widget>[
-                      SettingsGroup(
-                        items: getList(),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
-      } else {
-        _list.add(Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: createList(),
-            ),
-          ),
-        ));
-      }
-      return _list;
-    }
-
     return (Platform.isIOS)
         ? Scaffold(
             appBar: CupertinoNavigationBar(
@@ -98,8 +25,7 @@ class GradesListState extends State<GradesList> {
                 style: Styles.navBarTitle,
               ),
             ),
-            body: getColumnChildren()[0]
-          )
+            body: getColumnChildren()[0])
         : SafeArea(
             child: Scaffold(
               body: Column(
@@ -138,5 +64,78 @@ class GradesListState extends State<GradesList> {
       _items.add(_listItem);
     }
     return _items;
+  }
+
+  List<Widget> getColumnChildren() {
+    List<Widget> _list = List<Widget>();
+    if (!Platform.isIOS) {
+      _list.add(
+        Container(
+          color: Colors.orange,
+          child: Row(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                child: OutlineButton(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 3, bottom: 3),
+                      child: Icon(
+                        Icons.arrow_back,
+                        size: 36,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(15),
+                    )),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }),
+              ),
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 3, bottom: 3),
+                      child: Text(
+                        "Notenliste",
+                        style: TextStyle(fontSize: 25),
+                      ))),
+            ],
+          ),
+        ),
+      );
+    }
+    if (Platform.isIOS) {
+      _list.add(Container(
+        color: Styles.scaffoldBackground,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverSafeArea(
+              top: true,
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  <Widget>[
+                    SettingsGroup(
+                      items: getList(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ));
+    } else {
+      _list.add(Expanded(
+        child: SingleChildScrollView(
+          child: Column(
+            children: createList(),
+          ),
+        ),
+      ));
+    }
+    return _list;
   }
 }
