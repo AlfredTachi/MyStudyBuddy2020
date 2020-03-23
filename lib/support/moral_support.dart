bfabbb3a925025b45e4f6d1c2aeebb138ebe3be8
Jonas Abrahams <jonasabrahams@icloud.com>
2020-03-23 13:37:07 +0100

    moral support ios design

--- lib/support/moral_support.dart
+++ lib/support/moral_support.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:MyStudyBuddy2/theme/styles.dart';
 
 class MoralSupport extends StatefulWidget {
   @override
@@ -11,81 +14,115 @@ class MoralSupport extends StatefulWidget {
 class MoralSupportState extends State<MoralSupport> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-          child: Scaffold(
-        body: Column(
-          children: <Widget>[
+    return (Platform.isIOS)
+        ? Scaffold(
+            appBar: CupertinoNavigationBar(
+              actionsForegroundColor: CupertinoColors.activeOrange,
+              middle: Text(
+                "Es wird alles gut!",
+                style: Styles.navBarTitle,
+              ),
+            ),
+            body: Column(children: getColumnChildren()),
+          )
+        : SafeArea(
+            child: Scaffold(
+              body: Column(
+                children: getColumnChildren(),
+              ),
+            ),
+          );
+  }
+
+  List<Widget> getColumnChildren() {
+    List<Widget> _list = List<Widget>();
+    if (!Platform.isIOS) {
+      _list.add(
         Container(
           color: Colors.orange,
           child: Row(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: OutlineButton(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 3, bottom: 3),
-                          child: Icon(
-                            Icons.arrow_back,
-                            size: 36,
-                          ),
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(15),
-                          bottomRight: Radius.circular(15),
-                        )),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        }),
-                  ),
-                   Align(
-                      alignment: Alignment.topCenter,
-                      child: Padding(
-                          padding: const EdgeInsets.only(left:20, top: 3, bottom: 3),
-                          child: Text("Es wird alles gut!",style: TextStyle(fontSize: 25),))),
-                ],
-              ),
-        ),
-        Center(
-          child: FutureBuilder(
-            future: getDogImage(),
-            builder: (context, snap) {
-              if (snap.connectionState == ConnectionState.done) {
-                DogImage img = snap.data;
-                return Column(
-                  children: <Widget>[
-                    Container(
-                      width: 300,
-                      height: 500,
-                      child: Image.network(
-                        img.url,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                child: OutlineButton(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 3, bottom: 3),
+                      child: Icon(
+                        Icons.arrow_back,
+                        size: 36,
                       ),
                     ),
-                    RaisedButton(
-                        child: Text("Hilfe!"),
-                        onPressed: () {
-                          setState(() {});
-                        })
-                  ],
-                );
-              } else {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      CircularProgressIndicator(),
-                    ],
-                  ),
-                );
-              }
-            },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(15),
+                    )),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }),
+              ),
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 20, top: 3, bottom: 3),
+                      child: Text(
+                        "Es wird alles gut!",
+                        style: TextStyle(fontSize: 25),
+                      ))),
+            ],
           ),
         ),
-          ],
+      );
+    }
+    _list.add(
+      Center(
+        child: FutureBuilder(
+          future: getDogImage(),
+          builder: (context, snap) {
+            if (snap.connectionState == ConnectionState.done) {
+              DogImage img = snap.data;
+              return Column(
+                children: <Widget>[
+                  Container(
+                    width: 300,
+                    height: 500,
+                    child: Image.network(
+                      img.url,
+                    ),
+                  ),
+                  (Platform.isIOS)
+                      ? CupertinoButton(
+                          child: Text("Hilfe!"),
+                          color: CupertinoColors.activeOrange,
+                          onPressed: () {
+                            setState(() {});
+                          })
+                      : RaisedButton(
+                          child: Text("Hilfe!"),
+                          onPressed: () {
+                            setState(() {});
+                          })
+                ],
+              );
+            } else {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    (Platform.isIOS)
+                        ? CupertinoActivityIndicator()
+                        : CircularProgressIndicator(),
+                  ],
+                ),
+              );
+            }
+          },
         ),
       ),
     );
+    return _list;
   }
 
   Future<DogImage> getDogImage() async {
