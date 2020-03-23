7753af3d481799215e6b9599f2d41cc0f70fdb92
Jonas Abrahams <jonasabrahams@icloud.com>
2020-03-23 14:06:18 +0100

    privacy policy ios design

--- lib/support/privacyPolice/privacyPolice.dart
+++ lib/support/privacyPolice/privacyPolice.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:MyStudyBuddy2/theme/ios_quick_access_icons.dart';
+import 'package:MyStudyBuddy2/ios_list_design/cupertino_settings_group.dart';
+import 'package:MyStudyBuddy2/ios_list_design/cupertino_settings_icon.dart';
+import 'package:MyStudyBuddy2/theme/styles.dart';
 
 class PrivacyPolice extends StatefulWidget {
   @override
@@ -9,48 +15,78 @@ class PrivacyPolice extends StatefulWidget {
 class PrivacyPoliceState extends State<PrivacyPolice> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-          child: Scaffold(
-          body: Column(
-            children: <Widget>[
-              Container(
-                color: Colors.orange,
-                child: Row(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: OutlineButton(
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 3, bottom: 3),
-                            child: Icon(
-                              Icons.arrow_back,
-                              size: 36,
-                            ),
-                          ),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(15),
-                            bottomRight: Radius.circular(15),
-                          )),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          }),
-                    ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20, top: 3, bottom: 3),
-                        child: Text("Datenschutzerklärung",
-                            style: TextStyle(fontSize: 25)),
-                      ),
-                    )
-                  ],
-                ),
+    return (Platform.isIOS)
+        ? Scaffold(
+            appBar: CupertinoNavigationBar(
+              actionsForegroundColor: CupertinoColors.activeOrange,
+              middle: Text(
+                "Datenschutzerklärung",
+                style: Styles.navBarTitle,
               ),
-              Expanded(child: SingleChildScrollView(child: Column(children:createList()),))
-            ],
-          )),
-    );
+            ),
+            body: CustomScrollView(
+              slivers: <Widget>[
+                SliverSafeArea(
+                  top: false,
+                  sliver: SliverList(
+                    delegate: SliverChildListDelegate(
+                      <Widget>[
+                        SettingsGroup(
+                          items: getList(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        : SafeArea(
+            child: Scaffold(
+                body: Column(
+              children: <Widget>[
+                Container(
+                  color: Colors.orange,
+                  child: Row(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: OutlineButton(
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 3, bottom: 3),
+                              child: Icon(
+                                Icons.arrow_back,
+                                size: 36,
+                              ),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(15),
+                              bottomRight: Radius.circular(15),
+                            )),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                      ),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, top: 3, bottom: 3),
+                          child: Text("Datenschutzerklärung",
+                              style: TextStyle(fontSize: 25)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                    child: SingleChildScrollView(
+                  child: Column(children: createList()),
+                ))
+              ],
+            )),
+          );
   }
 
   List<Widget> createList() {
@@ -83,4 +119,52 @@ class PrivacyPoliceState extends State<PrivacyPolice> {
 
     return _items;
   }
+
+  List<SettingsItem> getList() {
+    return <SettingsItem>[
+      SettingsItem(
+        icon: SettingsIcon(
+          icon: IOSQuickAccessIcons.hslogo,
+          backgroundColor: Styles.hsBlue,
+        ),
+        label: "Hochschule",
+        content: SettingsNavigationIndicator(),
+        onPress: () {
+          _launchURL("https://atlas.ai.it.hs-worms.de/datenschutz/");
+        },
+      ),
+      SettingsItem(
+        icon: SettingsIcon(
+          icon: Icons.settings,
+          backgroundColor: CupertinoColors.systemBlue,
+        ),
+        label: "Studierendenwerk",
+        subtitle: "Mensaplan",
+        content: SettingsNavigationIndicator(),
+        onPress: () {
+          _launchURL("https://stw-vp.de/de/datenschutz/");
+        },
+      ),
+      SettingsItem(
+        icon: SettingsIcon(
+          icon: IOSQuickAccessIcons.lsflogo,
+          backgroundColor: CupertinoColors.systemGrey,
+        ),
+        label: "LSF",
+        content: SettingsNavigationIndicator(),
+        onPress: () {
+          _launchURL("https://www.hs-worms.de/fileadmin/media/SG2/Informieren/Datenschutz/Datenschutzinformationen_aktuell.pdf");
+        },
+      ),
+    ];
+  }
+
+    void _launchURL(String link) async {
+    if (await canLaunch(link)) {
+      await launch(link);
+    } else {
+      throw 'Could not launch $link';
+    }
+  }
+
 }
