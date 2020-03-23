8be02909873315732b3c46284ff58efa7a0084f4
Jonas Abrahams <jonasabrahams@icloud.com>
2020-03-23 18:12:12 +0100

    studyprogress ios design

--- lib/studyprogress/staggered_view_studyprogress/staggered_view_studyprogress.dart
+++ lib/studyprogress/staggered_view_studyprogress/staggered_view_studyprogress.dart
@@ -1,8 +1,12 @@
+import 'package:MyStudyBuddy2/theme/ios_quick_access_icons.dart';
+import 'package:MyStudyBuddy2/theme/styles.dart';
 import 'package:MyStudyBuddy2/dashboard/profile_page/achievement/achievement.dart';
 import 'package:MyStudyBuddy2/model/module.dart';
 import 'package:MyStudyBuddy2/singleton/module_controller.dart';
 import 'package:MyStudyBuddy2/singleton/profile_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
 
 class StaggeredView extends StatefulWidget {
   @override
@@ -16,73 +20,106 @@ class _StaggeredViewState extends State<StaggeredView> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.orange,
-              child: Row(
+    return (Platform.isIOS)
+        ? Scaffold(
+            appBar: CupertinoNavigationBar(
+              actionsForegroundColor: CupertinoColors.activeOrange,
+              middle: Text(
+                "Studienverlauf",
+                style: Styles.navBarTitle,
+              ),
+              trailing: Builder(
+                builder: (BuildContext context) {
+                  return CupertinoButton(
+                    padding: EdgeInsets.zero,
+                    child: Icon(
+                      IOSQuickAccessIcons.square_and_arrow_down,
+                      color: CupertinoColors.activeOrange,
+                      size: 25,
+                    ),
+                    onPressed: () {
+                      _downloadLSFData(context).whenComplete(() {
+                        Achievement().showAchievement(context, 8);
+                      });
+                    },
+                  );
+                },
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Column(children: generateSemesterTiles()),
+            ),
+          )
+        : SafeArea(
+            child: Scaffold(
+              body: Column(
                 children: <Widget>[
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: OutlineButton(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 3, bottom: 3),
-                          child: Icon(
-                            Icons.arrow_back,
-                            size: 36,
-                          ),
-                        ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(15),
-                          bottomRight: Radius.circular(15),
-                        )),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        }),
-                  ),
-                  Align(
-                      alignment: Alignment.topCenter,
-                      child: Padding(
-                          padding: const EdgeInsets.only(
-                              left: 20, top: 3, bottom: 3),
-                          child: Text(
-                            "Studienverlauf",
-                            style: TextStyle(fontSize: 25),
-                          ))),
-                  Expanded(
-                    child: Align(
-                        alignment: Alignment.topRight,
-                        child: Builder(builder: (BuildContext context) {
-                          scaffoldContext = context;
-                          return IconButton(
-                              icon: Icon(Icons.cloud_download),
-                              iconSize: 36,
+                  Container(
+                    color: Colors.orange,
+                    child: Row(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: OutlineButton(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 3, bottom: 3),
+                                child: Icon(
+                                  Icons.arrow_back,
+                                  size: 36,
+                                ),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(15),
+                                bottomRight: Radius.circular(15),
+                              )),
                               onPressed: () {
-                                _downloadLSFData(context).whenComplete(() {
-                                  Achievement().showAchievement(context, 8);
-                                });
-                              });
-                        })),
+                                Navigator.of(context).pop();
+                              }),
+                        ),
+                        Align(
+                            alignment: Alignment.topCenter,
+                            child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 20, top: 3, bottom: 3),
+                                child: Text(
+                                  "Studienverlauf",
+                                  style: TextStyle(fontSize: 25),
+                                ))),
+                        Expanded(
+                          child: Align(
+                              alignment: Alignment.topRight,
+                              child: Builder(builder: (BuildContext context) {
+                                scaffoldContext = context;
+                                return IconButton(
+                                    icon: Icon(Icons.cloud_download),
+                                    iconSize: 36,
+                                    onPressed: () {
+                                      _downloadLSFData(context)
+                                          .whenComplete(() {
+                                        Achievement()
+                                            .showAchievement(context, 8);
+                                      });
+                                    });
+                              })),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Container(
+                        child: Column(
+                          children: generateSemesterTiles(),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  child: Column(
-                    children: generateSemesterTiles(),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          );
   }
 
   List<Widget> generateSemesterTiles() {
@@ -100,15 +137,20 @@ class _StaggeredViewState extends State<StaggeredView> {
         _semesterTiles.add(semester(i));
       }
     }
+    if (Platform.isIOS) {
+      _semesterTiles.add(Container(height: 25,));
+    }
     return _semesterTiles;
   }
 
   Widget semester(int titleIndex) {
     String title = titleIndex.toString() + ". Semester";
     return Padding(
-      padding: const EdgeInsets.only(top: 5, bottom: 5, left: 3, right: 3),
+      padding: (Platform.isIOS)
+          ? const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0)
+          : const EdgeInsets.only(top: 5, bottom: 5, left: 3, right: 3),
       child: Material(
-        elevation: 15,
+        elevation: (Platform.isIOS) ? 0 : 15,
         borderRadius: BorderRadius.circular(15.0),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -120,10 +162,12 @@ class _StaggeredViewState extends State<StaggeredView> {
                   alignment: Alignment.topLeft,
                   child: Container(
                     child: Text(title,
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF013D62),
-                        )),
+                        style: (Platform.isIOS)
+                            ? Styles.studyprogressCardTitle
+                            : TextStyle(
+                                fontSize: 20,
+                                color: Color(0xFF013D62),
+                              )),
                   ),
                 ),
                 Expanded(
