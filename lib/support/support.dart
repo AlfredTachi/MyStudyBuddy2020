8348ade9e83e13c7119e0c6c00646b8d4abf06f4
Julia Henrich <inf2671@hs-worms.de>
2020-03-25 12:17:25 +0100

    first try support infos

--- lib/support/support.dart
+++ lib/support/support.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mailer/flutter_mailer.dart';
+import 'package:package_info/package_info.dart';
 
 class Support extends StatefulWidget {
   @override
@@ -10,7 +14,7 @@ class SupportState extends State<Support> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-          child: Scaffold(
+      child: Scaffold(
           body: Column(
         children: <Widget>[
           Container(
@@ -38,9 +42,9 @@ class SupportState extends State<Support> {
                 ),
                 Align(
                   alignment: Alignment.topCenter,
-                                child: Padding(
+                  child: Padding(
                     padding: const EdgeInsets.only(left: 20, top: 3, bottom: 3),
-                    child: Text("Hilfe",style:TextStyle(fontSize: 25)),
+                    child: Text("Hilfe", style: TextStyle(fontSize: 25)),
                   ),
                 )
               ],
@@ -56,27 +60,28 @@ class SupportState extends State<Support> {
 
   List<Widget> createList() {
     final MailOptions mailOptions = MailOptions(
-      subject: 'Support My Study Buddy',
-      recipients: ['inf2671@hs-worms.de'],
+      subject:
+          "Support My Study Buddy 2 " + getOperatingSystem() + getAppVersion(),
+      recipients: ["inf2671@hs-worms.de"],
     );
     List<Widget> _items = [
       ListTile(
         leading: Icon(Icons.help),
-        title: Text('FAQ', style: TextStyle(fontSize: 20)),
+        title: Text("FAQ", style: TextStyle(fontSize: 20)),
         onTap: () {
-          Navigator.pushNamed(context, '/supportMain/FAQ');
+          Navigator.pushNamed(context, "/supportMain/FAQ");
         },
       ),
       ListTile(
         leading: Icon(Icons.book),
-        title: Text('Schnelleinstieg', style: TextStyle(fontSize: 20)),
+        title: Text("Schnelleinstieg", style: TextStyle(fontSize: 20)),
         onTap: () {
-          Navigator.pushNamed(context, '/supportMain/quickaccess');
+          Navigator.pushNamed(context, "/supportMain/quickaccess");
         },
       ),
       ListTile(
           leading: Icon(Icons.mail),
-          title: Text('E-Mail Support', style: TextStyle(fontSize: 20)),
+          title: Text("E-Mail Support", style: TextStyle(fontSize: 20)),
           onTap: () async {
             try {
               await FlutterMailer.send(mailOptions);
@@ -84,19 +89,67 @@ class SupportState extends State<Support> {
           }),
       ListTile(
           leading: Icon(Icons.info),
-          title: Text('Impressum', style: TextStyle(fontSize: 20)),
+          title: Text("Impressum", style: TextStyle(fontSize: 20)),
           onTap: () {
-            Navigator.pushNamed(context, '/supportMain/impressum');
+            Navigator.pushNamed(context, "/supportMain/impressum");
           }),
       ListTile(
           leading: Icon(Icons.lock),
-          title: Text('Datenschutz', style: TextStyle(fontSize: 20)),
+          title: Text("Datenschutz", style: TextStyle(fontSize: 20)),
           onTap: () {
             Navigator.pushNamed(
-                context, '/supportMain/privacyPolice/privacyPolice');
+                context, "/supportMain/privacyPolice/privacyPolice");
           }),
     ];
 
     return _items;
   }
+
+  String getAppVersion() {
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      String version = packageInfo.version;
+      return version;
+    });
+  }
+
+  String getOperatingSystem() {
+    if (Platform.isAndroid) {
+      DeviceInfoPlugin().androidInfo.then((var androidInfo) {
+        var release = androidInfo.version.release;
+        var sdkInt = androidInfo.version.sdkInt;
+        return "Android" + release + "SDK" + sdkInt.toString();
+      });
+    } else if (Platform.isIOS) {
+      DeviceInfoPlugin().iosInfo.then((var iosInfo) {
+        var systemName = iosInfo.systemName;
+        var version = iosInfo.systemVersion;
+        return systemName + version;
+      });
+    } else {
+      return "";
+    }
+  }
+
+  /* Future<String> getAppVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String version = packageInfo.version;
+    return version;
+  }
+
+  Future<String> getOperatingSystem() async {
+    if (Platform.isAndroid) {
+      var androidInfo = await DeviceInfoPlugin().androidInfo;
+      var release = androidInfo.version.release;
+      var sdkInt = androidInfo.version.sdkInt;
+      return "Android" + release + "SDK" + sdkInt.toString();
+    } else if (Platform.isIOS) {
+      var iosInfo = await DeviceInfoPlugin().iosInfo;
+      var systemName = iosInfo.systemName;
+      var version = iosInfo.systemVersion;
+      return systemName + version;
+    } else {
+      return "";
+    }
+  }*/
+
 }
